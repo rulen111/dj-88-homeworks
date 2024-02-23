b20c40a00561535575fe1fb816b57b2f751cc72b
rulen111 <akhmarovri@yandex.ru>
2024-02-23 15:29:52 +0300

    Task completed

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,7 +1,11 @@
 import csv
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 from phones.models import Phone
+
+from django.utils.text import slugify
+import decimal
 
 
 class Command(BaseCommand):
@@ -14,4 +18,13 @@ class Command(BaseCommand):
 
         for phone in phones:
             # TODO: Добавьте сохранение модели
-            pass
+            entry = Phone(
+                id=phone["id"],
+                name=phone["name"],
+                price=decimal.Decimal(phone["price"]),
+                image=phone["image"],
+                release_date=datetime.strptime(phone["release_date"], "%Y-%m-%d").date(),
+                lte_exists=phone["lte_exists"],
+                slug=slugify(phone["name"])
+            )
+            entry.save()
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from phones.models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,26 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get("sort", "")
+    if sort:
+        order_key = {
+            "name": "name",
+            "min_price": "price",
+            "max_price": "-price"
+        }
+        phones = Phone.objects.order_by(order_key[sort])
+    else:
+        phones = Phone.objects.all()
+
+    context = {
+        "phones": phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+        "phone": Phone.objects.filter(slug=slug)[0]
+    }
     return render(request, template, context)
